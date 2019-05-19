13fb4be7e511c41853a736f0dd8171a65a0ca198
Oliver Bestwalter <oliver.bestwalter@avira.com>
2019-05-19 16:31:27 +0200

    use src layout and simplify setup.py

--- setup.py
+++ setup.py
@@ -2,49 +2,44 @@ import itertools
 from setuptools import setup, find_packages
 
 
-def main():
-    setup(
-        name="mau-mau",
-        author="Oliver Bestwalter",
-        url="https://github.com/obestwalter/mau-mau",
-        use_scm_version=True,
-        setup_requires=["setuptools_scm", "pytest-runner"],
-        tests_require=["pytest"],
-        packages=find_packages(),
-        license="MIT",
-        install_requires=["fire"],
-        extras_require=generate_extras_require(),
-        python_requires=">=3.6",
-        entry_points={
-            "console_scripts": [
-                "mau-mau = mau_mau.cli:main",
-                "mau-mau-statistics = mau_mau.statistics:main",
-            ]
-        },
-        classifiers=[
-            "Programming Language :: Python :: 3.6",
-            "Development Status :: 5 - Mature",
-            "Environment :: Console",
-            "Intended Audience :: Developers",
-            "Intended Audience :: End Users/Desktop",
-            "Operating System :: POSIX",
-            "Operating System :: Microsoft :: Windows",
-            "License :: OSI Approved :: MIT License",
-            "Topic :: Education",
-            "Topic :: Games/Entertainment :: Turn Based Strategy",
-        ],
-    )
-
-
 def generate_extras_require():
     extras = {
         ':sys_platform == "win32"': ["win_unicode_console"],
-        "test": ["tox", "flake8", "pytest", "pytest-watch"],
+        "test": ["flake8", "pytest", "pytest-watch"],
         "docs": ["mkdocs", "mkdocs-material"],
     }
     extras.update(dict(all=list(itertools.chain(*extras.values()))))
     return extras
 
 
-if __name__ == "__main__":
-    main()
+setup(
+    name="mau-mau",
+    author="Oliver Bestwalter",
+    url="https://github.com/obestwalter/mau-mau",
+    license="MIT",
+    use_scm_version=True,
+    python_requires=">=3.6",
+    setup_requires=["setuptools_scm", "pytest-runner"],
+    install_requires=["fire"],
+    extras_require=generate_extras_require(),
+    packages=find_packages('src'),
+    package_dir={'': 'src'},
+    entry_points={
+        "console_scripts": [
+            "mau-mau = mau_mau.cli:main",
+            "mau-mau-statistics = mau_mau.statistics:main",
+        ]
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3.6",
+        "Development Status :: 5 - Mature",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: End Users/Desktop",
+        "Operating System :: POSIX",
+        "Operating System :: Microsoft :: Windows",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Education",
+        "Topic :: Games/Entertainment :: Turn Based Strategy",
+    ],
+)
